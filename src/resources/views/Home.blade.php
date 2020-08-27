d94607c3f5ea25aae20586ba443a17e43d3f4f4d
Takeyama Takashi <takashi.takeyama@eustylelab.co.jp>
2020-08-27 13:28:19 +0900

    :sparkles: create-people-table

---
+++ src/resources/views/Home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Top</title>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+               Hello World
+            </div>
+        </div>
+    </div>
+</body>
+</html>
